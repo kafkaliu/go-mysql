2a760b9bbb853baf1b656a7cf305f949d69070da
liufei <liufei@actionsky.com>
2013-08-21 10:26:23 +0800

    mysql extract and install

--- src/mysql/mysqlserver.go
+++ src/mysql/mysqlserver.go
@@ -1,25 +1,43 @@
 package mysql
 
 import (
+	"errors"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"strings"
 	"util"
 )
 
-func UncompressAndRemoveData(installPackage string, optionFile string) error {
+const (
+	mysqlservice = "mysql-ha"
+)
+
+func Extract(installPackage string, optionFile string) error {
 	if props, err := LoadOptions(optionFile); err == nil {
 		switch {
 		case strings.HasSuffix(installPackage, ".zip"):
+			if _, err = os.Stat(props["mysqld"]["basedir"]); err == nil {
+				return errors.New("Fail to extract due to base dir exists.")
+			}
+			if _, err = os.Stat(props["mysqld"]["datadir"]); err == nil {
+				return errors.New("Fail to extract due to data dir exists.")
+			}
 			basedir := props["mysqld"]["basedir"]
 			log.Printf("install package from %s to %s", installPackage, basedir)
 			if err = os.Mkdir(basedir, 0644); err != nil {
 				return err
 			}
-			if err = util.Unzip(installPackage, basedir); err != nil {
+			options := map[string]int{"skip-components": 1}
+			if err = util.Unzip(installPackage, basedir, options); err != nil {
 				return err
 			}
-			os.RemoveAll(props["mysqld"]["datadir"])
+			b, _ := ioutil.ReadFile(optionFile)
+			os.MkdirAll(filepath.Join(basedir, "etc"), 0644)
+			ioutil.WriteFile(filepath.Join(basedir, "etc", "my.ini"), b, 0644)
 		case strings.HasSuffix(installPackage, ".tar.gz"):
 
 		}
@@ -27,4 +45,132 @@ func UncompressAndRemoveData(installPackage string, optionFile string) error {
 	} else {
 		return err
 	}
+}
+
+func mysql(basedir, sql string) error {
+	return execute(filepath.Join(basedir, "bin", "mysql"), "-uroot", "-h127.0.0.1", "-e", sql)
+}
+
+func execute(name string, arg ...string) error {
+	out, err := exec.Command(name, arg...).Output()
+	log.Printf("%s", out)
+	log.Printf("%v", err)
+	return err
+}
+
+func InstallServer(basedir string, datadir string, dataurl string, defaultdata string) error {
+	if _, err := os.Stat(datadir); err == nil {
+		return errors.New("Fail to extract due to data dir exists.")
+	}
+	os.MkdirAll(datadir, 0644)
+	if dataurl == "" {
+		options := map[string]int{"skip-components": 1}
+		if err := util.Unzip(defaultdata, datadir, options); err != nil {
+			return err
+		}
+	} else {
+		// todo: import data from dataurl
+	}
+
+	switch runtime.GOOS {
+	case "windows":
+		defaultsFile := filepath.Join(basedir, "etc", "my.ini")
+		if err := execute(filepath.Join(basedir, "bin", "mysqld"), "--install", "mysql-ha", "--defaults-file="+defaultsFile); err != nil {
+			return err
+		}
+
+		rpl_semi_sync_master_enabled := "0"
+		rpl_semi_sync_slave_enabled := "0"
+		if props, err := LoadOptions(defaultsFile); err == nil {
+			if props["mysqld"]["rpl_semi_sync_master_enabled"] == "1" {
+				rpl_semi_sync_master_enabled = "1"
+				delete(props["mysqld"], "rpl_semi_sync_master_enabled")
+			}
+			if props["mysqld"]["rpl_semi_sync_slave_enabled"] == "1" {
+				rpl_semi_sync_slave_enabled = "1"
+				delete(props["mysqld"], "rpl_semi_sync_slave_enabled")
+			}
+			if err = SaveOptions(defaultsFile, defaultsFile, props); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+		// log.Printf("rpl_semi_sync_master_enabled = %s, rpl_semi_sync_slave_enabled = %s", rpl_semi_sync_master_enabled, rpl_semi_sync_slave_enabled)
+
+		if err := Start(basedir); err != nil {
+			return err
+		}
+		if err := mysql(basedir, "delete from mysql.user where user=''"); err != nil {
+			return err
+		}
+		if err := mysql(basedir, "grant replication slave, replication client on *.* to repl identified by 'repl'"); err != nil {
+			return err
+		}
+		if err := mysql(basedir, "grant replication slave, replication client on *.* to 'repl'@'127.0.0.1' identified by 'repl'"); err != nil {
+			return err
+		}
+		if err := mysql(basedir, "install plugin rpl_semi_sync_master soname 'semisync_master.dll'"); err != nil {
+			return err
+		}
+		if err := mysql(basedir, "install plugin rpl_semi_sync_slave soname 'semisync_slave.dll'"); err != nil {
+			return err
+		}
+		if err := mysql(basedir, "set global rpl_semi_sync_master_enabled = 1"); err != nil {
+			return err
+		}
+		if err := mysql(basedir, "set global rpl_semi_sync_slave_enabled = 1"); err != nil {
+			return err
+		}
+
+		if props, err := LoadOptions(defaultsFile); err == nil {
+			props["mysqld"]["rpl_semi_sync_master_enabled"] = rpl_semi_sync_master_enabled
+			props["mysqld"]["rpl_semi_sync_slave_enabled"] = rpl_semi_sync_slave_enabled
+			if err = SaveOptions(defaultsFile, defaultsFile, props); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+
+	default:
+	}
+	// switch runtime.GOOS == "windows" {
+
+	// }
+	return nil
+}
+
+func UninstallServer(basedir string, datadir string) error {
+	Stop(basedir)
+	defer os.RemoveAll(basedir)
+	defer os.RemoveAll(datadir)
+	switch runtime.GOOS {
+	case "windows":
+		if err := execute("sc", "delete", mysqlservice); err != nil {
+			return err
+		}
+	default:
+	}
+	return nil
+}
+
+func Start(basedir string) error {
+	switch runtime.GOOS {
+	case "windows":
+		if err := execute("net", "start", mysqlservice); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Stop(basedir string) error {
+	switch runtime.GOOS {
+	case "windows":
+		if err := execute("net", "stop", mysqlservice); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- src/mysql/mysqlserver_test.go
+++ src/mysql/mysqlserver_test.go
@@ -5,13 +5,15 @@ import (
 	"log"
 	. "mysql"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
-func TestUncompressAndRemoveData(t *testing.T) {
+func TestExtract(t *testing.T) {
 	defer os.RemoveAll("c:/temp/mysql-install")
 	defer os.RemoveAll("c:/temp/mysql-data")
-	if err := UncompressAndRemoveData("testdata/mysql-5.5.33-winx64.zip", "testdata/my.ini"); err != nil {
+	if err := Extract("testdata/mysql-5.5.33-winx64.zip", "testdata/my.ini"); err != nil {
 		t.Fatalf("Fail to uncompress mysql.", err)
 	}
 	if _, err := os.Stat("c:/temp/mysql-install"); err != nil {
@@ -20,9 +22,26 @@ func TestUncompressAndRemoveData(t *testing.T) {
 	if _, err := os.Stat("c:/temp/mysql-data"); err == nil {
 		t.Fatalf("Should remove data dir.")
 	}
-	if content, err := ioutil.ReadFile("c:/temp/mysql-install/mysql-5.5.33-winx64/README"); err == nil {
+	if content, err := ioutil.ReadFile("c:/temp/mysql-install/etc/my.ini"); err == nil {
 		log.Println(string(content))
 	} else {
+		t.Error(err)
+	}
+}
+
+func TestExtractInstallUninstallServer(t *testing.T) {
+	defer UninstallServer("c:/temp/mysql-install", "c:/temp/mysql-data")
+	if err := Extract("testdata/mysql-5.5.33-winx64.zip", "testdata/my.ini"); err != nil {
+		t.Fatalf("Fail to uncompress mysql.", err)
+	}
+	InstallServer("c:/temp/mysql-install", "c:/temp/mysql-data", "", "testdata/mysql-5.5.33-data-winx64.zip")
+	if content, err := ioutil.ReadFile("c:/temp/mysql-install/etc/my.ini"); err == nil {
+		log.Println(string(content))
+	} else {
+		t.Error(err)
+	}
+	if out, err := exec.Command(filepath.Join("c:/temp/mysql-install", "bin", "mysql"), "-uroot", "-h127.0.0.1", "-e", "select 1").Output(); err != nil {
+		log.Println(out)
 		t.Error(err)
 	}
 }
